bf12634477802f2e109918a8db925d1bd185d070
Gautam Gupta <gautam23062007@gmail.com>
2025-07-19 10:22:47 +0530

    Strings

---
+++ DataStructure/Strings/Strings/StringsBasic.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class StringsBasic {
+    public static void main(String[] args) {
+        // String declaration
+
+        //  Eg: String name = "Tony";
+        // String Full name = "Tony Stark";
+        //   String sentence = "My name is Tony Stark";
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("Your name is:" + name);
+
+    }
+}
