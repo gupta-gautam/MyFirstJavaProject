d6b8d2b1dbd57c2103dd810bde99f2a278475498
Gautam Gupta <gautam23062007@gmail.com>
2025-08-05 23:50:15 +0530

    OOPS

--- DataStructure/OOPS/OOPS/Inheritance.java
+++ DataStructure/OOPS/OOPS/Inheritance.java
@@ -27,6 +27,12 @@ class Circle extends Shape {
     }
 }
 
+class Square extends Shape {
+    public void area(int r) {
+        System.out.println(r * r);
+    }
+}
+
 public class Inheritance {
     public static void main(String[] args) {
         Triangle t1 = new Triangle();
