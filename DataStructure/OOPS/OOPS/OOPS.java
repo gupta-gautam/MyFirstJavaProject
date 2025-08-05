dab171b521415109aaefe7d036fe83f6384fb76c
Gautam Gupta <gautam23062007@gmail.com>
2025-08-05 23:44:21 +0530

    OOPS

--- DataStructure/OOPS/OOPS/OOPS.java
+++ DataStructure/OOPS/OOPS/OOPS.java
@@ -30,11 +30,11 @@ public class OOPS {
     public static void main(String[] args) {
         Pen pen1 = new Pen();
         pen1.color = "blue";
-        pen1.type = "gel";
+        pen1.type = "gel pen";
 
         Pen pen2 = new Pen();
         pen2.color = "black";
-        pen1.type = "ballpoint";
+        pen1.type = "ballpoint pen";
 
         // pen1.printColor();
         //  pen2.printColor();
