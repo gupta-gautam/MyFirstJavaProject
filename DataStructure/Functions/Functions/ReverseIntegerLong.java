a75f0f97b7000dbc900f6579c317a44975247186
Gautam Gupta <gautam23062007@gmail.com>
2025-08-08 14:04:57 +0530

    Question

--- DataStructure/Functions/Functions/ReverseIntegerLong.java
+++ DataStructure/Functions/Functions/ReverseIntegerLong.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class ReverseInteger {
+public class ReverseIntegerLong {
 
     public static int solution(int x) {
 
--- DataStructure/Sorting/Sorting/Basics.java
+++ DataStructure/Sorting/Sorting/Basics.java
@@ -1,6 +1,7 @@
 
 public class Basics {
     public static void main(String[] args) {
-
+        System.out.println("hello");
+        System.out.println(3);
     }
 }
