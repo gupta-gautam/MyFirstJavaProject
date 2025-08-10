0b925b19fcb3aadae45e8dca791845ee1b123cb2
Gautam Gupta <gautam23062007@gmail.com>
2025-08-10 16:38:20 +0530

    Recursion

--- DataStructure/Recursion/Recursion/FibonacciSequence.java
+++ DataStructure/Recursion/Recursion/FibonacciSequence.java
@@ -1,9 +1,20 @@
 public class FibonacciSequence {
-    public static void printFibonacci(int n){
-
+    public static void printFibonacci(int a, int b, int n) {
+        if (n == 0) {
+            return;
+        }
+        int c = a + b;
+        System.out.println(c);
+        printFibonacci(b,c,n-1);
     }
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        System.out.println(a);
+        System.out.println(b);
+        int n=7;
+        printFibonacci(a, b, n-2);
     }
 }
 
