9663cc462fbe22937d7ec16e0a8604a721e2a44e
Gautam Gupta <ggautamg23@gmail.com>
2026-03-26 14:16:25 +0530

    Linked List

---
+++ DataStructure/LinkedList/LL/SingularLL.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+class LL {
+    Node head;
+
+    class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+
+    public void printList() {
+        if (head == null) {
+            System.out.print("list is empty");
+            return;
+        }
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + "->");
+            currNode = currNode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public void reverseIterate() {
+        Node prevNode = head;
+        Node currNode = head.next;
+        while (currNode != null) {
+            Node newNode = currNode.next;
+            currNode.next = prevNode;
+
+            //updation
+            prevNode = currNode;
+            currNode = newNode;
+        }
+        head.next = null;
+        head = prevNode;
+    }
+
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        list.printList();
+
+        list.reverseIterate();
+        list.printList();
+    }
+}
---
+++ DataStructure/LinkedList/LL/basicLL.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class basicLL {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+        list.addFirst("a");
+        list.addFirst("is");
+        System.out.println(list);
+    }
+}
