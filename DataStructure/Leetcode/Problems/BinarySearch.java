1d2e872fdeab12152a227b0fd63c3929027de5af
Gautam Gupta <ggautamg23@gmail.com>
2026-08-06 18:44:08 +0530

    Binary Search

---
+++ DataStructure/Leetcode/Problems/BinarySearch.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class BinarySearch {
+    public static int search(int nums[], int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left)/2;
+            if(nums[mid] ==  target){
+                return mid;
+            }
+            if(nums[mid] < target){
+                left = mid +1;
+            }else{
+                right = mid -1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int nums[] = new int[x];
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int target = sc.nextInt();
+        int ans = search(nums, target);
+        System.out.println(ans);
+    }
+}
