9a7841fc1b48aef7d06c73e195a4f1cbccc5d404
Gautam Gupta <ggautamg23@gmail.com>
2026-08-09 14:00:26 +0530

    LevelOrder Traversal

--- DataStructure/Trees/trees/BinaryTrees.java
+++ DataStructure/Trees/trees/BinaryTrees.java
@@ -38,6 +38,7 @@ public class BinaryTrees {
         preOrder(root.right);
     }
 
+
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
