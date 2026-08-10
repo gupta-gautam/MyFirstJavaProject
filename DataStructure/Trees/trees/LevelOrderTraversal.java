55024b2fd9b9ab1585e8d50a9bdacb09721e9ef4
Gautam Gupta <ggautamg23@gmail.com>
2026-08-10 19:41:11 +0530

    Level order

--- DataStructure/Trees/trees/LevelOrderTraversal.java
+++ DataStructure/Trees/trees/LevelOrderTraversal.java
@@ -58,6 +58,7 @@ public class LevelOrderTraversal {
         }
     }
 
+
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         LevelOrder tree = new LevelOrder();
