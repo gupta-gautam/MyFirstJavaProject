330a8b8e1ad8f1b1d38b570426ecb2a9ea38aaa1
Gautam Gupta <ggautamg23@gmail.com>
2026-08-15 00:37:14 +0530

    Diameter of Tree

--- DataStructure/Trees/trees/BinaryTrees.java
+++ DataStructure/Trees/trees/BinaryTrees.java
@@ -37,16 +37,18 @@ public class BinaryTrees {
         preOrder(root.left);
         preOrder(root.right);
     }
-    public static int countNodes(Node root){
-        if(root == null){
+
+    public static int countNodes(Node root) {
+        if (root == null) {
             return 0;
         }
         int left = countNodes(root.left);
         int right = countNodes(root.right);
-        return left + right +1;
+        return left + right + 1;
     }
-    public static int sumNodes(Node root){
-        if(root == null){
+
+    public static int sumNodes(Node root) {
+        if (root == null) {
             return 0;
         }
         int left = sumNodes(root.left);
@@ -54,12 +56,34 @@ public class BinaryTrees {
         return left + right + root.data;
     }
 
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        int myHeight = Math.max(leftHeight, rightHeight) + 1;
+        return myHeight;
+    }
+
+    public static int diameter(Node root) {
+        if (root == null) {
+            return 0;
+        }
+        int diam1 = diameter(root.left);
+        int diam2 = diameter(root.right);
+        int diam3 = height(root.left) + height(root.right) + 1;
+        return Math.max(diam3, Math.max(diam2, diam1));
+    }
+
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         //preOrder(root);
-        System.out.println(countNodes(root));
-        System.out.println(sumNodes(root));
+        //System.out.println(countNodes(root));
+        //System.out.println(sumNodes(root));
+        System.out.println(height(root));
+        System.out.println(diameter(root));
     }
 }
