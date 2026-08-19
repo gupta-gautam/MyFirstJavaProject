5c11050bdbddac67cf06244f4e4aa910b2d47f76
Gautam Gupta <ggautamg23@gmail.com>
2026-08-19 19:35:44 +0530

    Sum Of Left Leaves

---
+++ DataStructure/Trees/trees/SumOfLeftLeaves.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class SumOfLeftLeaves {
+    static class Node {
+        int data;
+        BinaryTrees.Node left;
+        BinaryTrees.Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
+}
