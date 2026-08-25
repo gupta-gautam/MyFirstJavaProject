7ed792aa4ed0f61c093c125e5cca376b479ebf06
Gautam Gupta <ggautamg23@gmail.com>
2026-08-25 18:57:54 +0530

    Search in BST

--- DataStructure/Trees/BinarySearchTrees/binarySearchTree/BST.java
+++ DataStructure/Trees/BinarySearchTrees/binarySearchTree/BST.java
@@ -24,22 +24,42 @@ public class BST {
         return root;
     }
 
-    public static void inorder(Node root){
-        if(root == null){
+    public static void inorder(Node root) {
+        if (root == null) {
             return;
         }
         inorder(root.left);
         System.out.println(root.data + " ");
         inorder(root.right);
     }
+
+    public static boolean search(Node root, int val) {
+        if (root == null) {
+            return false;
+        }
+        if (root.data > val) {
+            return search(root.left, val);
+        } else if (root.data == val) {
+            return true;
+        } else {
+            return search(root.right, val);
+        }
+    }
+
     public static void main(String[] args) {
-        int values[] = {5, 1, 3, 4, 2, 7};
+        int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
         Node root = null;
 
-        for (int i=0;i<values.length;i++){
-            root = insert(root,values[i]);
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
         }
         inorder(root);
         System.out.println();
+
+        if (search(root, 1)) {
+            System.out.println("found");
+        } else {
+            System.out.println("not found");
+        }
     }
 }
